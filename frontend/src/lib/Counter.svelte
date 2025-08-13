e5d13bfe91bfdd73e8ee31ad425dfdac8147c3ef
Konrad Klawonn <k.klawonn@mailbox.org>
2025-08-13 11:17:23 +0200

    Simplify Template and Show DaisyUI

--- frontend/src/lib/Counter.svelte
+++ frontend/src/lib/Counter.svelte
@@ -1,10 +1,26 @@
 <script lang="ts">
-  let count: number = $state(0)
-  const increment = () => {
-    count += 1
-  }
+    import { Greet } from "../../wailsjs/go/main/App";
+
+    let greeting: string = $state("This is Wails!");
+    let name: string = $state("");
+    const greet = () => {
+        Greet(name).then((result) => (greeting = result));
+    };
 </script>
 
-<button onclick={increment}>
-  count is {count}
-</button>
+<div class="hero">
+    <div class="hero-content flex-col">
+        <p class="self-center mt-5 text-2xl">{greeting}</p>
+        <div>
+            <input
+                class="input w-full m-10"
+                placeholder="Enter name here..."
+                bind:value={name}
+            />
+
+            <button class="btn btn-primary mx-10 w-full" onclick={greet}>
+                Greet
+            </button>
+        </div>
+    </div>
+</div>
